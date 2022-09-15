c9967dd2cfdfeb0b1c67841924c8d62bda3a3770
lathomasexport <laurent.thomas@cern.ch>
2022-09-15 13:46:14 +0200

    Update path to set environment in condor submission script

--- Macros/L1scripts/scriptcondor.sh
+++ Macros/L1scripts/scriptcondor.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd /user/lathomas/L1Studies/SampleGeneration/SingleNeutrinoPU1/CMSSW_12_2_1/src
+cd ~/Run3DQM/CMSSW_12_4_3/src/JetMETStudies/Macros/L1scripts/
 cmsenv 
-cd /user/lathomas/MacrosforM/L1Studies/L1NtuplesAnalysis/SandBox
+
 
 python3 analyzel1.py --maxEvents -1 -i $1 -o $2 -c $3
 
