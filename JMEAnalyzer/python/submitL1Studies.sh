9ba74c3d6d1e56a155cff205872a9518dfec1561
lathomasexport <laurent.thomas@cern.ch>
2022-10-07 14:10:58 +0200

    Submission commands for L1 studies

---
+++ JMEAnalyzer/python/submitL1Studies.sh
@@ -0,0 +1,49 @@
+
+#sh SubmitToCrab.sh /EGamma/Run2022B-PromptReco-v1/MINIAOD egm_forjme_2022b 1 L1Study_SinglePhotonforJME DataRun3 
+#sh SubmitToCrab.sh /EGamma/Run2022C-PromptReco-v1/MINIAOD egm_forjme_2022c 1 L1Study_SinglePhotonforJME DataRun3 
+#sh SubmitToCrab.sh /EGamma/Run2022D-PromptReco-v1/MINIAOD egm_forjme_2022dv1 1 L1Study_SinglePhotonforJME DataRun3 
+#sh SubmitToCrab.sh /EGamma/Run2022D-PromptReco-v2/MINIAOD egm_forjme_2022dv2 1 L1Study_SinglePhotonforJME DataRun3 
+
+ 
+sh SubmitToCrab.sh /EGamma/Run2022B-PromptReco-v1/MINIAOD egm_zee_2022b 1 L1Study_ZToEE DataRun3 
+sh SubmitToCrab.sh /EGamma/Run2022C-PromptReco-v1/MINIAOD egm_zee_2022c 1 L1Study_ZToEE DataRun3 
+sh SubmitToCrab.sh /EGamma/Run2022D-PromptReco-v1/MINIAOD egm_zee_2022dv1 1 L1Study_ZToEE DataRun3 
+sh SubmitToCrab.sh /EGamma/Run2022D-PromptReco-v2/MINIAOD egm_zee_2022dv2 1 L1Study_ZToEE DataRun3 
+
+#sh SubmitToCrab.sh /SingleMuon/Run2022B-PromptReco-v1/MINIAOD singlemuon_forjme_2022b  1 L1Study_SingleMuforJME DataRun3 
+#sh SubmitToCrab.sh /SingleMuon/Run2022C-PromptReco-v1/MINIAOD singlemuon_forjme_2022c  1 L1Study_SingleMuforJME DataRun3 
+#sh SubmitToCrab.sh /Muon/Run2022C-PromptReco-v1/MINIAOD muon_forjme_2022c  1 L1Study_SingleMuforJME DataRun3 
+#sh SubmitToCrab.sh /Muon/Run2022D-PromptReco-v1/MINIAOD muon_forjme_2022dv1  1 L1Study_SingleMuforJME DataRun3 
+#sh SubmitToCrab.sh /Muon/Run2022D-PromptReco-v2/MINIAOD muon_forjme_2022dv2  1 L1Study_SingleMuforJME DataRun3 
+
+
+sh SubmitToCrab.sh /SingleMuon/Run2022B-PromptReco-v1/MINIAOD singlemuon_zmumu_2022b  1 L1Study_ZToMuMu DataRun3
+sh SubmitToCrab.sh /SingleMuon/Run2022C-PromptReco-v1/MINIAOD singlemuon_zmumu_2022c  1 L1Study_ZToMuMu DataRun3
+sh SubmitToCrab.sh /Muon/Run2022C-PromptReco-v1/MINIAOD muon_zmumu_2022c  1 L1Study_ZToMuMu DataRun3 
+sh SubmitToCrab.sh /Muon/Run2022D-PromptReco-v1/MINIAOD muon_zmumu_2022dv1  1 L1Study_ZToMuMu DataRun3 
+#sh SubmitToCrab.sh /Muon/Run2022D-PromptReco-v2/MINIAOD muon_zmumu_2022dv2  1 L1Study_ZToMuMu DataRun3 
+
+sh SubmitToCrab.sh /DYJetsToLL_M-50_TuneCP5_13p6TeV-madgraphMLM-pythia8/Run3Winter22MiniAOD-122X_mcRun3_2021_realistic_v9_ext1-v1/MINIAODSIM dymumu 1 L1Study_ZToMuMu DataRun3
+sh SubmitToCrab.sh /DYJetsToLL_M-50_TuneCP5_13p6TeV-madgraphMLM-pythia8/Run3Winter22MiniAOD-122X_mcRun3_2021_realistic_v9_ext1-v1/MINIAODSIM dyee 1 L1Study_ZToEE DataRun3
+
+sh SubmitToCrab.sh /DYJetsToLL_M-50_TuneCP5_13p6TeV-madgraphMLM-pythia8/Run3Winter22MiniAOD-122X_mcRun3_2021_realistic_v9_ext1-v1/MINIAODSIM dymumu 1 L1Study_SingleMuforJME DataRun3
+
+
+#2018  
+
+sh SubmitToCrab.sh /SingleMuon/Run2018A-UL2018_MiniAODv2-v3/MINIAOD muon_zmumu_2018a 1 L1Study_ZToMuMu DataUL2018A
+sh SubmitToCrab.sh /SingleMuon/Run2018B-UL2018_MiniAODv2-v2/MINIAOD muon_zmumu_2018b 1 L1Study_ZToMuMu DataUL2018B
+sh SubmitToCrab.sh /SingleMuon/Run2018C-UL2018_MiniAODv2-v2/MINIAOD muon_zmumu_2018c 1 L1Study_ZToMuMu DataUL2018C
+sh SubmitToCrab.sh /SingleMuon/Run2018D-UL2018_MiniAODv2-v3/MINIAOD muon_zmumu_2018d 1 L1Study_ZToMuMu DataUL2018D
+
+sh SubmitToCrab.sh /SingleMuon/Run2018A-UL2018_MiniAODv2-v3/MINIAOD muon_mujet_2018a 1 L1Study_SingleMuforJME DataUL2018A
+sh SubmitToCrab.sh /SingleMuon/Run2018B-UL2018_MiniAODv2-v2/MINIAOD muon_mujet_2018b 1 L1Study_SingleMuforJME DataUL2018B
+sh SubmitToCrab.sh /SingleMuon/Run2018C-UL2018_MiniAODv2-v2/MINIAOD muon_mujet_2018c 1 L1Study_SingleMuforJME DataUL2018C
+sh SubmitToCrab.sh /SingleMuon/Run2018D-UL2018_MiniAODv2-v3/MINIAOD muon_mujet_2018d 1 L1Study_SingleMuforJME DataUL2018D
+
+sh SubmitToCrab.sh /EGamma/Run2018A-UL2018_MiniAODv2-v1/MINIAOD egm_photonjet_2018a 1 L1Study_SinglePhotonforJME DataUL2018A
+sh SubmitToCrab.sh /EGamma/Run2018B-UL2018_MiniAODv2-v1/MINIAOD egm_photonjet_2018b 1 L1Study_SinglePhotonforJME DataUL2018B
+sh SubmitToCrab.sh /EGamma/Run2018C-UL2018_MiniAODv2-v1/MINIAOD egm_photonjet_2018c 1 L1Study_SinglePhotonforJME DataUL2018C
+sh SubmitToCrab.sh /EGamma/Run2018D-UL2018_MiniAODv2-v2/MINIAOD egm_photonjet_2018d 1 L1Study_SinglePhotonforJME DataUL2018D
+
+
